2327ee72f48bc80bb1a302114dcdb8f42833570c
EJGDLG <93065287+EJGDLG@users.noreply.github.com>
2023-01-13 12:18:37 -0600

    Add files via upload

---
+++ Radio/IProductividad.java
@@ -0,0 +1,9 @@
+public interface Productividad {
+     /* ========================
+        MODO PRODUCTIVIDAD
+     =========================*/
+
+     ArrayList<String> tarjetas = new ArrayList<>();
+    /*Imprime las tarjetas en tarjetas[]*/
+    void verTarjetas(ArrayList<String> tarjetas);
+}
