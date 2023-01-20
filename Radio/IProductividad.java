d1ab63a47cb60ca27d8a059f56af643e2cf4ab37
EJGDLG <93065287+EJGDLG@users.noreply.github.com>
2023-01-20 11:11:55 -0600

    Delete Radio directory

--- Radio/IProductividad.java
+++
@@ -1,9 +0,0 @@
-public interface Productividad {
-     /* ========================
-        MODO PRODUCTIVIDAD
-     =========================*/
-
-     ArrayList<String> tarjetas = new ArrayList<>();
-    /*Imprime las tarjetas en tarjetas[]*/
-    void verTarjetas(ArrayList<String> tarjetas);
-}
